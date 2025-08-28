0f3b82dae2cd0917ef6de585c8f2a12bbad5ec78
Norton Acosta <87377260+nortonacosta@users.noreply.github.com>
2025-08-28 16:36:34 -0300

    Curso Finalizado

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('galeria.urls')),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
+    
     
 ]
