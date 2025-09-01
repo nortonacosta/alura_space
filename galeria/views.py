49eb6e218772ce1e01cbd5ddb34ced7bc777dc81
Norton Acosta <87377260+nortonacosta@users.noreply.github.com>
2025-09-01 16:58:50 -0300

    feat: nomes dinamicos index.html

--- galeria/views.py
+++ galeria/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+
 def index(request):
-        return render(request, 'galeria/index.html')
+        
+        dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "Webbtelescope.org / NASA/ James Webb",
+            },
+        2: {"nome": "Galáxia NGC 1079",
+            "legenda": "nasa.org / NASA/ Hubble",}
+        
+        }
+        
+        return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
         return render(request, 'galeria/imagem.html')
