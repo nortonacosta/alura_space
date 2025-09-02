27bc1889ffe6eeaba336476f528744399b75aa49
Norton Acosta <87377260+nortonacosta@users.noreply.github.com>
2025-09-02 16:10:06 -0300

    feat: criando banco de dados

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,6 +4,7 @@ from galeria.views import index, imagem
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/', imagem, name='imagem')
+    #... outras rotas podem ser adicionadas aqui
     
     
 ]
