1003bbea5674e6aec621a31eaea01390998384a0
Norton Acosta <87377260+nortonacosta@users.noreply.github.com>
2025-09-04 16:35:09 -0300

    feat: passando referencia

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,7 +3,7 @@ from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem')
     #... outras rotas podem ser adicionadas aqui
     
     
