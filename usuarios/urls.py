047dd31744af5d14b506669759634805b77c7d66
Norton Acosta <87377260+nortonacosta@users.noreply.github.com>
2025-09-24 14:37:55 -0300

    feat: Introduzindo login e cadastro-

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from usuarios.views import login, cadastro
 
 
 
 urlpatterns = [
-    
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
     
 ]
